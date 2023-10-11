f1602cae5b7be00537a154a0f068c920c74f5012
morjun <anbndn5@gmail.com>
2023-10-11 21:43:52 +0900

    Change SEP to END and shorten the code

--- bertTrain.py
+++ bertTrain.py
@@ -12,6 +12,8 @@ import pandas as pd
 import numpy as np
 import random
 import time
+import os
+import json
 import datetime
 
 def main():
@@ -31,13 +33,14 @@ def tokenize(csvFile: str):
     truncFrame = fullFrame.loc[:, ['sourceDataInfo.newsTitle', 'sourceDataInfo.newsContent', 'labeledDataInfo.clickbaitClass']]
     truncFrame = truncFrame.rename(columns={"sourceDataInfo.newsTitle": "title", "sourceDataInfo.newsContent": "content", "labeledDataInfo.clickbaitClass": "label"})
 
-    truncFrame['content'] = truncFrame['content'].str.replace(r'\\n', ' ', regex=True)
-    truncFrame['content'] = truncFrame['content'].str.replace(r'\\[^a-z]', '', regex=True)
-    truncFrame['title'] = truncFrame['title'].str.replace(r'\\n', ' ', regex=True)
-    truncFrame['title'] = truncFrame['title'].str.replace(r'\\[^a-z]', '', regex=True)
+    truncFrame['content'] = truncFrame['content'].str.replace(r'\\n', ' ', regex=True).replace(r'\\[^a-z]', '', regex=True).replace('[.,!?-]', '')
+    truncFrame['title'] = truncFrame['title'].str.replace(r'\\n', ' ', regex=True).replace(r'\\[^a-z]', '', regex=True).replace('[.,!?-]', '')
 
-    content_bert = ["[CLS] " + str(sent) + " [SEP]" for sent in truncFrame['content']]
-    title_bert = ["[CLS] " + str(sent) + " [SEP]" for sent in truncFrame['title']]
+    content_bert = ["[CLS] " + str(sent) + " [END]" for sent in truncFrame['content']]
+    title_bert = ["[CLS] " + str(sent) + " [END]" for sent in truncFrame['title']]
+    
+    # [SEP]는 전체 내용의 끝이 아니라 문장과 문장 사이에 붙여야 한다.
+    # https://neptune.ai/blog/how-to-code-bert-using-pytorch-tutorial
 
     tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased', do_lower_case=False)
     tokenized_contents = [tokenizer.tokenize(sent) for sent in content_bert]
@@ -53,16 +56,17 @@ def makeCsv():
         for file in files:
             if file.endswith(".json"):
                 with open(os.path.join(root, file), encoding="utf-8") as f:
-                    data = pandas.json_normalize(json.loads(f.read()))
+                    data = pd.json_normalize(json.loads(f.read()))
                     print(f"Processing {root}\\{file}: {data['sourceDataInfo.newsTitle'][0]}")
                     dfs.append(data)
                     
-    df = pandas.concat(dfs, ignore_index=True)
+    df = pd.concat(dfs, ignore_index=True)
     
     print(df)
     df.to_csv("data.csv", index=False)
-    
 
+def train(tokenCsv: str):
+    NotImplemented
 
 if __name__ == "__main__":
     main()
