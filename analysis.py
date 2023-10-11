decec8555dad399ff3a7d943cd4411a4ed8ed650
morjun <anbndn5@gmail.com>
2023-10-11 14:01:49 +0900

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -15,16 +15,30 @@ import time
 import datetime
 
 def main():
+    pd.set_option('display.max_columns', None)
+
     fullFrame = pd.read_csv("data.csv")
+
     print(fullFrame.shape)
     print(fullFrame.head())
     print(fullFrame.columns)
-    content_bert = ["[CLS] " + str(sent) + " [SEP]" for sent in fullFrame['sourceDataInfo.newsContent']]
+
+    truncFrame = fullFrame.loc[:, ['sourceDataInfo.newsTitle', 'sourceDataInfo.newsContent', 'labeledDataInfo.clickbaitClass']]
+    truncFrame = truncFrame.rename(columns={"sourceDataInfo.newsTitle": "title", "sourceDataInfo.newsContent": "content", "labeledDataInfo.clickbaitClass": "label"})
+
+    truncFrame['content'] = truncFrame['content'].str.replace(r'\\n', ' ', regex=True)
+    truncFrame['content'] = truncFrame['content'].str.replace(r'\\[^a-z]', '', regex=True)
+    truncFrame['title'] = truncFrame['title'].str.replace(r'\\n', ' ', regex=True)
+    truncFrame['title'] = truncFrame['title'].str.replace(r'\\[^a-z]', '', regex=True)
+
+    content_bert = ["[CLS] " + str(sent) + " [SEP]" for sent in truncFrame['content']]
+    title_bert = ["[CLS] " + str(sent) + " [SEP]" for sent in truncFrame['title']]
+
     tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased', do_lower_case=False)
-    tokenized_texts = [tokenizer.tokenize(sent) for sent in content_bert]
+    tokenized_contents = [tokenizer.tokenize(sent) for sent in content_bert]
+    tokenized_titles = [tokenizer.tokenize(sent) for sent in title_bert]
 
-    print(tokenized_texts[0])
-
+    print(tokenized_contents[0])
 
 
 if __name__ == "__main__":
